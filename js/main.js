bb2051e1ea532dae5ad7ef009e1a0cdaf7011034
salvatorebriga <salvo.z2302@gmail.com>
2024-04-29 23:35:35 +0200

    milestone 2 complete

--- js/main.js
+++ js/main.js
@@ -7,29 +7,44 @@ let discount = 0;
 /*funzione calcolatrice*/
 function myCalc(){
     let userName = document.getElementById('name').value;
-    console.log(userName);
+    document.getElementById('userName').innerHTML = userName;
     let userKilometers = parseInt(document.getElementById('kilometers').value);
-    console.log(userKilometers);
     let userAge = document.getElementById('age').value;
-    console.log(userAge);
 
     let price = userKilometers * priceForKm;
 
     if(userAge === 'underage'){
         discount = 20;
         price = price - ((price * discount) / 100);
-        console.log(price.toFixed(2) + '$' + ' 20% youth discount');
+        document.getElementById('userOffer').innerHTML = 'Underage Ticket';
+        document.getElementById('userCarriage').innerHTML = Math.floor(Math.random() * 10);
+        document.getElementById('userCpCode').innerHTML = Math.floor(Math.random() * 99999);
+        document.getElementById('userPrice').innerHTML = price.toFixed(2) + ' $';
     }else if(userAge === 'over65'){
         discount = 40;
         price = price - ((price * discount) / 100);
-        console.log(price.toFixed(2) + '$' + ' 40% discount over 65s');
+        document.getElementById('userOffer').innerHTML = 'Over 65 Ticket';
+        document.getElementById('userCarriage').innerHTML = Math.floor(Math.random() * 10);
+        document.getElementById('userCpCode').innerHTML = Math.floor(Math.random() * 99999);
+        document.getElementById('userPrice').innerHTML = price.toFixed(2) + ' $';
     }
     else{
-        console.log(price.toFixed(2) + '$' + ' no discounts applied')
+        document.getElementById('userOffer').innerHTML = 'Standard Ticket';
+        document.getElementById('userCarriage').innerHTML = Math.floor(Math.random() * 10);
+        document.getElementById('userCpCode').innerHTML = Math.floor(Math.random() * 99999);
+        document.getElementById('userPrice').innerHTML = price.toFixed(2) + ' $';
     }
 }
 
 /*funzione di reset*/
 function myReset(){
-    
+
+    document.getElementById('name').value = '';
+    document.getElementById('kilometers').value = '';
+
+    document.getElementById('userName').innerHTML = '- - - - - - - -';
+    document.getElementById('userOffer').innerHTML = '- - - -';
+    document.getElementById('userCarriage').innerHTML = '- - - -';
+    document.getElementById('userCpCode').innerHTML = '- - - -';
+    document.getElementById('userPrice').innerHTML = '- - - -';
 }
