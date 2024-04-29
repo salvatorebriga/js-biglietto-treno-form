f992746b7fc9ffb1926edd14c17b63bc14a48e06
salvatorebriga <salvo.z2302@gmail.com>
2024-04-29 22:01:26 +0200

    added variable assignment via form

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 "use strict"
 
 function myCalc(){
-
+    let userName = document.getElementById('name').value;
+    console.log(userName);
+    let userKilometers = document.getElementById('kilometers').value;
+    console.log(userKilometers);
+    let userAge = document.getElementById('age').value;
+    console.log(userAge);
 }
 
 function myReset(){
-    
+
 }
